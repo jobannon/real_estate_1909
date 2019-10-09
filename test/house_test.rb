ad087153900599ccfdba66d0dea1bcc5e5c9aab9
Joshua O'Bannon <josho@Joshuas-MacBook-Pro.local>
2019-10-09 09:55:33 -0600

    add to room class initialize method

--- test/house_test.rb
+++ test/house_test.rb
@@ -7,11 +7,14 @@ require './lib/room'
 class HouseTest < Minitest::Test
   def setup
     @house = House.new("$400000", "123 sugar lane")
+    @room_1 = Room.new(:bedroom, 10, 13)
+    @room_2 = Room.new(:bedroom, 11, 15)
   end
 
   def test_it_exists
     assert_equal "$400000", @house.price
     assert_equal "123 sugar lane", @house.address
-
+    assert_equal 0, @house.rooms.length
   end
+
 end
